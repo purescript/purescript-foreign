7cd85435b14e56d3eea78504aa880d773dfe29ae
Gary Burgess <gary.burgess@gmail.com>
2014-02-17 18:50:40 +0000

    Restore applicative style example

---
+++ examples/Applicative.purs
@@ -0,0 +1,20 @@
+module Applicative where
+
+  import Prelude
+  import Either
+  import Arrays
+  import Eff
+  import JSON
+  
+  data Point = Point Number Number Number
+  
+  instance JSON.ReadJSON Point where
+    readJSON = Point <$> readJSONProp "x" 
+                     <*> readJSONProp "y"
+                     <*> readJSONProp "z"
+      
+  main = do
+  
+    Trace.trace $ case parseJSON "{ \"x\": 1, \"y\": 2, \"z\": 3 }" of
+      Left err -> "Error parsing JSON:\n" ++ err
+      Right (Point x y z) -> show [x, y, z]
