37cd15137d4411d9b9b4ae9c5018aff5168c6183
Gary Burgess <gary.burgess@gmail.com>
2014-03-08 17:39:15 +0000

    Tweak showJSONImpl, readJSONArray

--- src/Data/JSON.purs
+++ src/Data/JSON.purs
@@ -17,7 +17,7 @@ import Control.Monad
 
 foreign import data JSON :: *
 
-foreign import fromString 
+foreign import fromString
   "function fromString (str) { \
   \  try { \
   \    return _ps.Data_Either.Right(JSON.parse(str)); \
@@ -26,7 +26,7 @@ foreign import fromString
   \  } \
   \}" :: String -> Either String JSON
 
-foreign import readPrimType 
+foreign import readPrimType
   "function readPrimType (typeName) { \
   \  return function (value) { \
   \    if (toString.call(value) == '[object ' + typeName + ']') { \
@@ -35,24 +35,21 @@ foreign import readPrimType
   \    return _ps.Data_Either.Left('Value is not a ' + typeName + ''); \
   \  }; \
   \}" :: forall a. String -> JSON -> Either String a
-                            
-foreign import readMaybe 
+
+foreign import readMaybe
   "function readMaybe (value) { \
   \  return value === undefined || value === null ? _ps.Data_Maybe.Nothing : _ps.Data_Maybe.Just(value); \
   \}" :: forall a. JSON -> Maybe JSON
-                            
-foreign import readProp 
+
+foreign import readProp
   "function readProp (k) { \
   \  return function (obj) { \
   \    return _ps.Data_Either.Right(obj[k]);\
   \  }; \
   \}" :: forall a. String -> JSON -> Either String JSON
 
-foreign import showJSONImpl 
-  "function showJSONImpl (obj) { \
-  \  return JSON.stringify(obj); \
-  \}" :: JSON -> String
-                    
+foreign import showJSONImpl "var showJSONImpl = JSON.stringify;" :: JSON -> String
+
 instance showJSON :: Prelude.Show JSON where
   show = showJSONImpl
 
@@ -72,42 +69,42 @@ instance applicativeJSONParser :: Prelude.Applicative JSONParser where
   (<*>) (JSONParser f) (JSONParser p) = JSONParser \x -> case f x of
       Left err -> Left err
       Right f' -> f' <$> p x
-    
+
 instance functorJSONParser :: Prelude.Functor JSONParser where
   (<$>) f (JSONParser p) = JSONParser \x -> f <$> p x
-  
+
 class ReadJSON a where
   readJSON :: JSONParser a
-  
+
 instance readJSONString :: ReadJSON String where
   readJSON = JSONParser $ readPrimType "String"
-  
+
 instance readJSONNumber :: ReadJSON Number where
   readJSON = JSONParser $ readPrimType "Number"
-  
+
 instance readJSONBoolean :: ReadJSON Boolean where
   readJSON = JSONParser $ readPrimType "Boolean"
-  
+
 instance readJSONArray :: (ReadJSON a) => ReadJSON [a] where
-  readJSON = (JSONParser $ readPrimType "Array") >>= \xs -> JSONParser \_ -> 
-    readJSONArrayItem `mapM` (zip (range 0 (length xs)) xs)
-  
-readJSONArrayItem :: forall a. (ReadJSON a) => Tuple Number JSON -> Either String a
-readJSONArrayItem (Tuple i x) = case runParser readJSON x of
-    Right result -> Right result
-    Left err -> Left $ "Error reading item at index " ++ (show i) ++ ":\n" ++ err
-  
+  readJSON =
+    let arrayItem (Tuple i x) = case runParser readJSON x of
+      Right result -> Right result
+      Left err -> Left $ "Error reading item at index " ++ (show i) ++ ":\n" ++ err
+    in
+    (JSONParser $ readPrimType "Array") >>= \xs -> JSONParser \_ ->
+      arrayItem `mapM` (zip (range 0 (length xs)) xs)
+
 instance readJSONMaybe :: (ReadJSON a) => ReadJSON (Maybe a) where
-  readJSON = (JSONParser $ Right <<< readMaybe) >>= \x -> JSONParser \_ -> 
+  readJSON = (JSONParser $ Right <<< readMaybe) >>= \x -> JSONParser \_ ->
     case x of
       Just x' -> runParser readJSON x' >>= return <<< Just
       Nothing -> return Nothing
 
 readJSONProp :: forall a. (ReadJSON a) => String -> JSONParser a
-readJSONProp p = (JSONParser \x -> readProp p x) >>= \x -> JSONParser \_ -> 
+readJSONProp p = (JSONParser \x -> readProp p x) >>= \x -> JSONParser \_ ->
   case runParser readJSON x of
     Right result -> Right result
     Left err -> Left $ "Error reading property '" ++ p ++ "':\n" ++ err
-    
+
 parseJSON :: forall a. (ReadJSON a) => String -> Either String a
 parseJSON json = fromString json >>= runParser readJSON
