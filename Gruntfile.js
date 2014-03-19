8e026c6c2b01a8c48033ff127457c280955b8e26
Gary Burgess <gary.burgess@gmail.com>
2014-03-19 23:04:12 +0000

    Add grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    "use strict";
+
+    grunt.initConfig({ 
+    
+        clean: ["externs", "js"],
+    
+        "purescript-make": {
+            options: {
+                tco: true,
+                magicDo: true
+            },
+            lib: {
+                src:
+                    [ "src/**/*.purs"
+                    , "bower_components/purescript-*/src/**/*.purs"
+                    ]
+            }
+        }
+        
+    });
+
+    grunt.loadNpmTasks("grunt-purescript");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.registerTask("default", ["purescript-make:lib"]);
+};
