5de1e92c055e9c7276841e9ab8cd3ef304ee73ef
Gary Burgess <gary.burgess@gmail.com>
2014-04-26 02:07:52 +0100

    Update build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,28 @@
 module.exports = function(grunt) {
 
-    "use strict";
+  "use strict";
 
-    grunt.initConfig({ 
+  grunt.initConfig({ 
+  
+    libFiles: [
+      "src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs",
+    ],
     
-        clean: ["externs", "js"],
-    
-        "purescript-make": {
-            options: {
-                tco: true,
-                magicDo: true
-            },
-            lib: {
-                src:
-                    [ "src/**/*.purs"
-                    , "bower_components/purescript-*/src/**/*.purs"
-                    ]
-            }
+    clean: ["output"],
+  
+    pscMake: {
+        all: {
+            src: ["examples/**/*.purs", "<%=libFiles%>"],
         }
-        
-    });
+    },
+    dotPsci: ["<%=libFiles%>"]
 
-    grunt.loadNpmTasks("grunt-purescript");
-    grunt.loadNpmTasks("grunt-contrib-clean");
+  });
 
-    grunt.registerTask("default", ["purescript-make:lib"]);
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-purescript");
+  
+  grunt.registerTask("make", ["pscMake", "dotPsci"]);
+  grunt.registerTask("default", ["make"]);
 };
