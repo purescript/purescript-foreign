b870fb725c23f19cef6d5d2e19d8e22658f7f83e
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-11 14:16:00 -0800

    Migrated FFI to ES modules via 'lebab'

--- src/Foreign/Keys.js
+++ src/Foreign/Keys.js
@@ -1,6 +1,6 @@
 "use strict";
 
-exports.unsafeKeys = Object.keys || function (value) {
+export var unsafeKeys = Object.keys || function (value) {
   var keys = [];
   for (var prop in value) {
     if (Object.prototype.hasOwnProperty.call(value, prop)) {
